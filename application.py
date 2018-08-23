a9a8466e1c55019b81063b69d0a2d9f224f2ac5e
Nicolas Sanchez <sancheznico93@gmail.com>
2018-08-23 08:57:08 -0300

    changed message

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return 'Hola, alumno del curso cloud y devops de verano!'
+    return 'Hola Gonza! Bienvenido a DinoCloud!'
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=5000)
