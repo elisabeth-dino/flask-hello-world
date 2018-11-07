6cfbe50f631cbf4082e18d3f25d76def3aea5b81
Leonardo <leo97ldg@gmail.com>
2018-11-07 12:58:23 -0300

    Quitando etiqueta html

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    
+   return 'Cambio de mensaje!'
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=8888)
