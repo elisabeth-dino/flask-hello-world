2d00d2c8b64a5d5ded4ae61e20d16bb87c13516c
elisabeth-dino <elisabeth.leonhardt@dinocloudconsulting.com>
2019-02-08 11:50:34 -0300

    first modification

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return 'Im in staging. i hope go to production'
+    return 'Im in staging. I hope go to production very soon'
 
 if __name__ == '__main__':
     application.run(host=os.getenv('HOST', '0.0.0.0'), port=os.getenv('PORT', '8888'))
